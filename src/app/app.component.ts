258a57baca8a70da61739dbb90016253c29e9772
purich purisinsits <yuyuisa@192-168-1-3.tpgi.com.au>
2019-04-20 03:52:03 +1000

    3rd routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,26 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  public user: any;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private angFireAuth: AngularFireAuth,
+    private router: Router
   ) {
     this.initializeApp();
+    this.checkAuthState();
   }
 
   initializeApp() {
@@ -22,5 +31,17 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  checkAuthState(){
+    this.angFireAuth.authState.subscribe( (user) => {
+      if(user){
+
+      }
+      else{
+        
+      }
+    });
+
   }
 }
